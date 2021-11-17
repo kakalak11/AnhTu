fe9f59ca6c817b9992433a157235fb3c6b092526
kakalak11 <92971472+kakalak11@users.noreply.github.com>
2021-11-17 15:58:26 +0700

    Update docsotien.js

--- session01/docsotien.js
+++ session01/docsotien.js
@@ -13,18 +13,35 @@ const numLetters = [
 ];
 const numPosition = ["vạn", "nghìn", "trăm", "mươi", "linh"];
 
-const number = 20406;
-let numberArray = Array.from(String(number), Number);
-if (numberArray.length != 6) {
-    for (let i = numberArray.length; i < 6; i++) {
-        numberArray.unshift(" ");
+let number = 213476;
+let number2 = 004367;
+let number3 = 123407;
+let number4 = 999999;
+
+// console.log(numberArray);
+
+// function convertToArray(number) {
+//     let numberArray = Array.from(String(number), Number);
+//     if (numberArray.length != 6) {
+//         for (let i = numberArray.length; i < 6; i++) {
+//             numberArray.unshift(" ");
+//         }
+//     }
+
+//     return numberArray;
+
+// }
+
+function docTien(number) {
+
+    let numberArray = Array.from(String(number), Number);
+    if (numberArray.length != 6) {
+        for (let i = numberArray.length; i < 6; i++) {
+            numberArray.unshift(" ");
+        }
     }
-}
-let stringArray = [];
 
-console.log(numberArray);
-
-function docTien(numberArray) {
+    let stringArray = [];
     for (let i = 0; i < 6; i++) {
 
         if (i === 0) {
@@ -40,6 +57,11 @@ function docTien(numberArray) {
                 stringArray.push(numLetters[numberArray[i]] + " " + numPosition[3]);
                 continue;
             }
+        }
+
+        if (i === 1 && numberArray[0] != 0 && numberArray[0] != 1) {
+            stringArray.push("mốt vạn ");
+            continue;
         }
 
         if (i === 4 && numberArray[i] === 0) {
@@ -60,16 +82,18 @@ function docTien(numberArray) {
 
         stringArray.push(numLetters[numberArray[i]] + " " + numPosition[i - 1]);
     }
+
+    for (let i = String(number).length; i < 6; i++) {
+        stringArray.shift();
+    }
+
+    let string = stringArray.join(" ");
+
+    return string;
 }
 
-docTien(numberArray);
-
-console.log(stringArray);
-for (let i = String(number).length; i < 6; i++) {
-    stringArray.shift();
-}
-
-let string = stringArray.join(" ");
-
-console.log(string);
+console.log(number + " đọc là: " + docTien(number));
+console.log(number2 + " đọc là: " + docTien(number2));
+console.log(number3 + " đọc là: " + docTien(number3));
+console.log(number4 + " đọc là: " + docTien(number4));
 
